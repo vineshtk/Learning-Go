71e63315bc079130605cbf66065a43530bb57876
Vinesh tk <vineshtk10@gmail.com>
2022-12-26 17:29:21 +0530

    code with user details

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
+// packaging the whole code in to main
 package main
 
+// importing fmt package
 import "fmt"
 
+// creating the main function
 func main() {
 
 	var conferenceName = "Go conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+
+	//simpler variable declatation
+	remainingTickets := 50
+
+	//using Ptintf
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("Total %v slots and %v slots are still available to book.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here!")
+
+	var userName string //user can input details here
+	var userTickets int
+
+	userName = "Tommy"
+	userTickets = 3
+
+	fmt.Printf("Thank you %v for showing your interest \n", userName)
+	fmt.Printf("You bought %v tickets for the event \n", userTickets)
+
+	//getting the variable type
+	fmt.Printf("The type of variable conferenceName is %T \n", conferenceName)
 
 }
