76c713e5ab130f85e1ac63e046bf30f62fd6b2cf
Vinesh tk <vineshtk10@gmail.com>
2022-12-26 16:22:46 +0530

    added variable

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!!")
+	fmt.Println("Welcome to our confrence booking application!")
+	fmt.Println("Get your tickets here!")
+
+	var conferenceName = "Go conference"
+	fmt.Println(conferenceName)
 }
